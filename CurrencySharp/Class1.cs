359b68862945c507ab9075f83af671f0e4ac82ff
Luke Warren <lukejkw@gmail.com>
2016-11-11 18:16:16 +0200

    Add project files.

---
+++ CurrencySharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencySharp
+{
+    public class Class1
+    {
+    }
+}
